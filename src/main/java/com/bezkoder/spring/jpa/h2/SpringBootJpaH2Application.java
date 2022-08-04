0f3a30c0c83592899f1aab25382cbcb4c786cb19
Sarvesh Dhawale <sarvesh.dhawale@confluxsys.com>
2022-08-04 17:33:47 +0530

    commit for this project first time

---
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -0,0 +1,15 @@
+package com.bezkoder.spring.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaH2Application.class, args);
+		System.out.println("Hello World");
+
+	}
+
+}
